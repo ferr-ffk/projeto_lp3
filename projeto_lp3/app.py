35c8ee713828efaef2c81ca6e3a050b29bc06704
Fernando Freitas <freitax.li@gmail.com>
2024-06-13 16:48:23 -0300

    continuação da aula e efetuação dos exercícios

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from validate_docbr import CPF, CNPJ
 
 app = Flask(__name__)
 
@@ -13,3 +14,41 @@ def pagina_contato():
 @app.route("/produtos")
 def pagina_produtos():
     return "<h1>produtos</h1>"
+
+# Exercicios 
+# /servicos retornar "Nossos servicos"
+# pagina /gerar-cpf retorna Cpf aleatório
+# pagina /gerar-cnpj retorna Cnpj aleatório
+
+@app.route("/servicos")
+def pagina_servicos():
+    return """
+    Nossos Serviços
+    <a href=\"/gerar-cpf\">Gerar Cpf</a>
+    <a href=\"/gerar-cnpj\">Gerar Cnpj</a>
+    <script>
+    
+        alert('bem vindoooo :3')
+
+    </script>
+    """
+
+@app.route("/somar/<int:Number>/<int:Number2>")
+def pagina_soma(Number, Number2):
+    return f"Soma de {Number} + {Number2} = {Number + Number2}"
+
+@app.route("/gerar-cpf")
+def pagina_gerar_cpf():
+    return CPF().generate(True)
+
+@app.route("/gerar-cnpj")
+def pagina_gerar_cnpj():
+    return CNPJ().generate(True)
+
+
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
